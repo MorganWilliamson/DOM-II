f431196a6339dc2fe4a5445c841c566a82a30609
MorganWilliamson <mwilliamson7695@gmail.com>
2020-09-29 15:21:14 -0500

    Added comment to top of JS file.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+/* Recommended options: 
+* `mouseover`
+* `keydown`
+* `wheel`
+* `load`
+* `focus`
+* `resize`
+* `scroll`
+* `select`
+* `dblclick`
+* `drag / drop`
+
+* Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
+   
+* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
+* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+*/
+
